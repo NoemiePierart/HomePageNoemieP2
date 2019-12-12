ece12e3c008aef44dbbbcbb4a5b283dc42c8f7bf
Noemie Pierart <noemie.pierart@gmail.com>
2019-12-12 18:03:43 +0100

    Added front

--- app/assets/javascripts/components/navbar.js
+++ app/assets/javascripts/components/navbar.js
@@ -1,13 +1,26 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 
+
 const classToggle =() => {
-  console.log("Coucou");
-  const navs = document.querySelectorAll('.Navbar__Items')
 
-  navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
-}
+jQuery(document).ready(function() {
+	jQuery('.toggle-nav').click(function(e) {
+		jQuery(this).toggleClass('active');
+		jQuery('.menu ul').toggleClass('active');
 
-document.querySelector('.Navbar__Link-toggle')
-  .addEventListener('click', classToggle);
+		e.preventDefault();
+	});
+});
 
 export { classToggle };
+
+// const classToggle =() => {
+
+// let mainNav = document.getElementById('js-menu');
+// let navBarToggle = document.getElementById('js-navbar-toggle');
+
+// navBarToggle.addEventListener('click', function () {
+//   mainNav.classList.toggle('active');
+// });
+
+// export { classToggle };
